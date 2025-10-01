ed76b5330cfd0c79d3260e3104db94e0cce4fac4
unknown <zurqulnan@gmail.com>
2025-10-01 07:37:59 -0700

    hasihng with map

---
+++ map-hashing.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int n;
+    cout<<"Enter the size of array";
+    cin>> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    //pre compute
+    map<int , int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cout<<"enter the number of queries";
+    cin>>q;
+    while (q--)
+    {
+        int number;
+        cout<<"enter the number to find its frequency";
+        cin>>number;
+        //fetch
+
+        cout<<"the frequency of"<<number<< "is"<<mpp[number]<<endl;
+        }
+    
+}
