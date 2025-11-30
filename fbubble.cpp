5e383f6b509885b64118097f004efc54e794d352
unknown <zurqulnan@gmail.com>
2025-11-30 10:59:04 +0500

    update

--- fbubble.cpp
+++ fbubble.cpp
@@ -1,22 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(vector<int> &arr){
-    int n = arr.size();
+// void bubbleSort(vector<int> &arr){
+//     int n = arr.size();
     
-    for(int i = n-1 ; i>=0; i--){
-        bool swapped = false;   // reset before each pass
+//     for(int i = n-1 ; i>=0; i--){
+//         bool swapped = false;   // reset before each pass
 
-        for(int j = 0; j < i; j++){
-            if(arr[j] > arr[j+1]){
+//         for(int j = 0; j < i; j++){
+//             if(arr[j] > arr[j+1]){
+//                 swap(arr[j], arr[j+1]);
+//                 swapped = true;
+//             }
+//         }
+//         if(!swapped) break;   // exit early if already sorted
+//     }
+// }
+void bubblesort(vector<int>&arr){
+    int n = arr.size();
+    for(int i = n-1; i>=0; i--){
+        bool swapped = false;
+        for(int j = 0 ; j<i ; j++){
+            if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j+1]);
-                swapped = true;
             }
         }
-        if(!swapped) break;   // exit early if already sorted
+        if(!swapped) break;
     }
 }
-    
 int main(){
     int n;
     cout<<"enter the n"<<endl;
@@ -27,9 +38,10 @@ int main(){
     for (int i = 0; i < n; i++)
         cin>>arr[i];
 
-    bubbleSort(arr);
+    bubblesort(arr);
 
     cout<<"sorted array:"<<endl;
     for (int i = 0; i < n; i++)
         cout<<arr[i]<<" ";
 }
+
